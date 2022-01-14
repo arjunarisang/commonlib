e6beee869214da581719036368541e1a1b60786b
Unggul Hasan <unggulhasan@gmail.com>
2022-01-14 13:36:32 +0700

    1.1.3-2.3.11-RELEASE

--- src/main/java/io/github/arjunarisang/commonlib/audit/AuditModel2.java
+++ src/main/java/io/github/arjunarisang/commonlib/audit/AuditModel2.java
@@ -9,49 +9,49 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-public abstract class AuditModel {
+public abstract class AuditModel2 {
 
     @CreatedDate
-    private LocalDateTime createdAt;
+    private LocalDateTime createdDate;
 
     @CreatedBy
     private String createdBy;
 
     @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedDate;
 
     @LastModifiedBy
     private String updatedBy;
 
-    public AuditModel() {
+    public AuditModel2() {
     }
 
-    public LocalDateTime getCreatedAt() {
-        return this.createdAt;
+    public LocalDateTime getCreatedDate() {
+        return this.createdDate;
     }
 
     public String getCreatedBy() {
         return this.createdBy;
     }
 
-    public LocalDateTime getUpdatedAt() {
-        return this.updatedAt;
+    public LocalDateTime getUpdatedDate() {
+        return this.updatedDate;
     }
 
     public String getUpdatedBy() {
         return this.updatedBy;
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
     }
 
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
 
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
     }
 
     public void setUpdatedBy(String updatedBy) {
@@ -60,18 +60,20 @@ public abstract class AuditModel {
 
     public boolean equals(final Object o) {
         if (o == this) return true;
-        if (!(o instanceof AuditModel)) return false;
-        final AuditModel other = (AuditModel) o;
+        if (!(o instanceof AuditModel2)) return false;
+        final AuditModel2 other = (AuditModel2) o;
         if (!other.canEqual((Object) this)) return false;
-        final Object this$createdAt = this.getCreatedAt();
-        final Object other$createdAt = other.getCreatedAt();
-        if (this$createdAt == null ? other$createdAt != null : !this$createdAt.equals(other$createdAt)) return false;
+        final Object this$createdDate = this.getCreatedDate();
+        final Object other$createdDate = other.getCreatedDate();
+        if (this$createdDate == null ? other$createdDate != null : !this$createdDate.equals(other$createdDate))
+            return false;
         final Object this$createdBy = this.getCreatedBy();
         final Object other$createdBy = other.getCreatedBy();
         if (this$createdBy == null ? other$createdBy != null : !this$createdBy.equals(other$createdBy)) return false;
-        final Object this$updatedAt = this.getUpdatedAt();
-        final Object other$updatedAt = other.getUpdatedAt();
-        if (this$updatedAt == null ? other$updatedAt != null : !this$updatedAt.equals(other$updatedAt)) return false;
+        final Object this$updatedDate = this.getUpdatedDate();
+        final Object other$updatedDate = other.getUpdatedDate();
+        if (this$updatedDate == null ? other$updatedDate != null : !this$updatedDate.equals(other$updatedDate))
+            return false;
         final Object this$updatedBy = this.getUpdatedBy();
         final Object other$updatedBy = other.getUpdatedBy();
         if (this$updatedBy == null ? other$updatedBy != null : !this$updatedBy.equals(other$updatedBy)) return false;
@@ -79,24 +81,24 @@ public abstract class AuditModel {
     }
 
     protected boolean canEqual(final Object other) {
-        return other instanceof AuditModel;
+        return other instanceof AuditModel2;
     }
 
     public int hashCode() {
         final int PRIME = 59;
         int result = 1;
-        final Object $createdAt = this.getCreatedAt();
-        result = result * PRIME + ($createdAt == null ? 43 : $createdAt.hashCode());
+        final Object $createdDate = this.getCreatedDate();
+        result = result * PRIME + ($createdDate == null ? 43 : $createdDate.hashCode());
         final Object $createdBy = this.getCreatedBy();
         result = result * PRIME + ($createdBy == null ? 43 : $createdBy.hashCode());
-        final Object $updatedAt = this.getUpdatedAt();
-        result = result * PRIME + ($updatedAt == null ? 43 : $updatedAt.hashCode());
+        final Object $updatedDate = this.getUpdatedDate();
+        result = result * PRIME + ($updatedDate == null ? 43 : $updatedDate.hashCode());
         final Object $updatedBy = this.getUpdatedBy();
         result = result * PRIME + ($updatedBy == null ? 43 : $updatedBy.hashCode());
         return result;
     }
 
     public String toString() {
-        return "AuditModel(createdAt=" + this.getCreatedAt() + ", createdBy=" + this.getCreatedBy() + ", updatedAt=" + this.getUpdatedAt() + ", updatedBy=" + this.getUpdatedBy() + ")";
+        return "AuditModel2(createdDate=" + this.getCreatedDate() + ", createdBy=" + this.getCreatedBy() + ", updatedDate=" + this.getUpdatedDate() + ", updatedBy=" + this.getUpdatedBy() + ")";
     }
 }
